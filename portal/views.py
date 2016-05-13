fb64b7be23824a129b1a5a53bd7082e8a1719580
Patrick Curry <patrick.curry@leibniz-zmt.de>
2016-05-13 16:11:26 +0200

    Django Templating for loop used to pass coordinates to template; there must be a much better way than this.

--- portal/views.py
+++ portal/views.py
@@ -10,4 +10,14 @@ def tester(request):
     """
     Display template with google maps javascript api tests.
     """
-    return render(request, 'portal/tester.html')
+    bremen = [53.0793, 8.8017]
+    indio = [33.7206, -116.2156]
+    honolulu = [21.3069, -157.8583]
+    cape_town = [-33.9249, 18.4241]
+    jakarta = [-6.1745, 106.8227]
+
+    locations = [bremen, indio, honolulu, cape_town, jakarta]
+    locations1 = ['bremen', 'indio', 'honolulu', 'cape_town', 'jakarta']
+    context = {'locations': locations}
+
+    return render(request, 'portal/tester.html', context)
