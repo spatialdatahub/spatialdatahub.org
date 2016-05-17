80fef599d0635883fe5442685a1f85c3479101fa
Patrick Curry <patrick.curry@leibniz-zmt.de>
2016-05-17 14:51:27 +0200

    Big move

--- main/urls.py
+++ main/urls.py
@@ -1,18 +1,3 @@
-"""main URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.9/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import url
 from django.contrib import admin
 
@@ -20,6 +5,26 @@ from portal import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.index),
-    url(r'^tester/$', views.tester),
+
+    url(r'^$', 
+        views.index,
+        name='index'),
+
+    url(r'^many_points_hardcoded_js/$',
+        views.many_points_hardcoded_js,
+        name='many_points_hardcoded_js'),
+
+    url(r'^many_points_hardcoded_py/$',
+        views.many_points_hardcoded_py,
+        name='many_points_hardcoded_py'),
+
+    url(r'^point_with_info_window/$',
+        views.point_with_info_window,
+         name='point_with_info_window'),
+
+    url(r'^many_points_with_info_windows/$',
+        views.many_points_with_info_windows,
+        name='many_points_with_info_windows'),
+
+
 ]
--- portal/views.py
+++ portal/views.py
@@ -1,4 +1,6 @@
+from django.core.urlresolvers import reverse
 from django.shortcuts import render
+
 
 def index(request):
     """
@@ -6,18 +8,88 @@ def index(request):
     """
     return render(request, 'portal/index.html')
 
-def tester(request):
+def point_with_info_window(request):
     """
-    Display template with google maps javascript api tests.
+    Display template with single marker and info window.
     """
-    bremen = [53.0793, 8.8017]
-    indio = [33.7206, -116.2156]
-    honolulu = [21.3069, -157.8583]
-    cape_town = [-33.9249, 18.4241]
-    jakarta = [-6.1745, 106.8227]
+    return render(request, 'portal/point_with_info_window.html')
+
+def many_points_hardcoded_js(request):
+    """
+    Display template with google maps javascript api and points hardcoded in the JavaScript script.
+    """
+    return render(request, 'portal/many_points_hardcoded_js.html')
+
+def many_points_hardcoded_py(request):
+    """
+    Display template with google maps javascript api and points hardcoded in this views file.
+    """
+    bremen = {'cityname':'Bremen', 'lat': 53.0793, 'lng': 8.8017}
+    indio = {'cityname':'Indio', 'lat': 33.7206, 'lng': -116.2156}
+    honolulu = {'cityname':'Honolulu', 'lat': 21.3069, 'lng': -157.8583}
+    cape_town = {'cityname': 'Cape Town', 'lat': -33.9249, 'lng': 18.4241}
+    jakarta = {'cityname': 'Jakarta', 'lat': -6.1745, 'lng': 106.8227}
 
     locations = [bremen, indio, honolulu, cape_town, jakarta]
-    locations1 = ['bremen', 'indio', 'honolulu', 'cape_town', 'jakarta']
     context = {'locations': locations}
 
-    return render(request, 'portal/tester.html', context)
+    return render(request, 'portal/many_points_hardcoded_py.html', context)
+
+def many_points_with_info_windows(request):
+    """
+    Display template with google maps javascript api and points hardcoded in this views file.
+    The hardcoded points in addition to cityname, latitude, and longitude  will have
+    description and wikipedia key: value pairs,
+    """
+
+    bremen_description = '''<div id="content"><div id="siteNotice"></div>
+        <h1 id="firstHeading" class="firstHeading">Bremen</h1>
+        <div id="bodyContent">
+        <p><b>Bremen</b>, is a hanseatic city in northern Germany</p>
+        <p><a href="https://en.wikipedia.org/wiki/Bremen">Bremen on Wikipedia</a></p>
+        </div></div>'''
+
+    indio_description = '''<div id="content"><div id="siteNotice"></div>
+        <h1 id="firstHeading" class="firstHeading">Indio</h1>
+        <div id="bodyContent">
+        <p><b>Indio</b>, is a desert city in southern California</p>
+        <p><a href="https://en.wikipedia.org/wiki/Indio,_California">Indio on Wikipedia</a></p>
+        </div></div>'''
+
+    honolulu_description = '''<div id="content"><div id="siteNotice"></div>
+        <h1 id="firstHeading" class="firstHeading">Honolulu</h1>
+        <div id="bodyContent">
+        <p><b>Honolulu</b>, is the most populous city in the Hawaiian Islands</p>
+        <p><a href="https://en.wikipedia.org/wiki/Honolulu">Honolulu on Wikipedia</a></p>
+        </div></div>'''
+
+    cape_town_description = '''<div id="content"><div id="siteNotice"></div>
+        <h1 id="firstHeading" class="firstHeading">Cape Town</h1>
+        <div id="bodyContent">
+        <p><b>Cape Town</b>, is a costal city in South Africa</p>
+        <p><a href="https://en.wikipedia.org/wiki/Cape_Town">Cape Town on Wikipedia</a></p>
+        </div></div>'''
+
+    jakarta_description = '''<div id="content"><div id="siteNotice"></div>
+        <h1 id="firstHeading" class="firstHeading">Jakarta</h1>
+        <div id="bodyContent">
+        <p><b>Jakarta</b>, is the capital of Indonesia</p>
+        <p><a href="https://en.wikipedia.org/wiki/Jakarta">Cape Town on Wikipedia</a></p>
+        </div></div>'''
+
+
+    bremen = {'cityname':'Bremen', 'lat': 53.0793, 'lng': 8.8017,
+              'description':bremen_description}
+    indio = {'cityname':'Indio', 'lat': 33.7206, 'lng': -116.2156,
+              'description':indio_description}
+    honolulu = {'cityname':'Honolulu', 'lat': 21.3069, 'lng': -157.8583,
+              'description':honolulu_description}
+    cape_town = {'cityname': 'Cape Town', 'lat': -33.9249, 'lng': 18.4241,
+              'description':cape_town_description}
+    jakarta = {'cityname': 'Jakarta', 'lat': -6.1745, 'lng': 106.8227,
+              'description':jakarta_description}
+
+    locations = [bremen, indio, honolulu, cape_town, jakarta]
+    context = {'locations': locations}
+
+    return render(request, 'portal/many_points_with_info_windows.html', context) 
