0b960f37742307bc0e0f577def98db70a745947c
Patrick Curry <patrick.curry@leibniz-zmt.de>
2016-08-30 11:14:13 +0200

    first working leaflet tests added

--- static/js/datasets/portal_map.js
+++ static/js/datasets/portal_map.js
@@ -1,25 +1,17 @@
-var myNamespace = (function(w, d, $) {
-	// w = window, d = document, $ = jQuery
+// set up the map
+var myMap;
+myMap = new L.Map('mapid')
 
-	// set up the map
-	var myMap;
-	myMap = new L.Map('mapid')
+// create base tile layer for map
+var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+var osm = new L.TileLayer(osmUrl, 
+	{
+	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>',
+	minZoom:0, 
+	maxZoom: 18
+	}
+);
 
-	// create base tile layer for map
-	var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-	var osm = new L.TileLayer(osmUrl, 
-		{
-		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>',
-		minZoom:0, 
-		maxZoom: 18
-		}
-	);
-
-	// set map view and add base layer
-	myMap.setView({lat: 0, lng: 8.8460}, 2);
-	myMap.addLayer(osm);
-
-})(window, document, jQuery);
-
-console.log(typeof myNamespace);
-console.log(typeof myMap);
+// set map view and add base layer
+myMap.setView({lat: 0, lng: 8.8460}, 2);
+myMap.addLayer(osm);
--- static/tests/tests.js
+++ static/tests/tests.js
@@ -1,18 +1,24 @@
-function isEven(val) {
-	return val % 2 === 0;
-}
-
-QUnit.test("isEven()", function( assert ) {
-	assert.ok(isEven(0), 'Zero is an even number');
-	assert.ok(!isEven(1), 'one is not an even number');
-	assert.ok(isEven(2), 'two is an even number');
-	assert.ok(isEven(-2), 'negative two is an even number');
+QUnit.test("Test that the map's center is in the correct place?", function( assert ) {
+	assert.equal(myMap.getCenter().toString(), "LatLng(0, 8.846)");
 });
 
-QUnit.test("Is the map center in the correct place?", function( assert ) {
-	assert.equal(myMap.getCenter().toString(), 'LatLng(0, 8.846)');
+QUnit.test("Test that the map's zoom correct?", function( assert ) {
+	assert.equal(myMap.getZoom(), 2);
 });
 
-QUnit.test("Is the base map layer correct", function( assert ) {
-	assert.equal(myMap.getLayers(), 'LatLng(0, 8.846)');
+QUnit.test("Test base layer (osm) URL", function( assert ) {
+	assert.equal(osm._url, "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png");
+});
+
+QUnit.test("Test base layer (osm) attribution?", function( assert ){
+	assert.equal(osm.options.attribution, 
+		'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>');
+});
+
+QUnit.test("Test base layer (osm) minZoom?", function( assert ){
+	assert.equal(osm.options.minZoom, 0);
+});
+
+QUnit.test("Test base layer (osm) maxZoom?", function( assert ){
+	assert.equal(osm.options.maxZoom, 18);
 });
