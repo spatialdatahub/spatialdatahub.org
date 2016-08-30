e2cca532e152599e22d25039a35f3a3875aff679
Patrick Curry <patrick.curry@leibniz-zmt.de>
2016-08-30 16:26:44 +0200

    fighting with map layers

--- static/js/datasets/portal_map.js
+++ static/js/datasets/portal_map.js
@@ -1,18 +1,42 @@
-// set up the map
-var myMap;
-myMap = new L.Map('mapid');
-
 // create base tile layer variable for map
-var 	osm = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+var osm = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 	minZoom:0, 
 	maxZoom: 19 
 });
 
-// set map view and add base layer
-myMap.setView({lat: 0, lng: 8.8460}, 2);
-myMap.addLayer(osm);
+
+// create layer group and add base tile layer to layer group
+var baseLayer = L.layerGroup();
+baseLayer.addLayer(osm);
+
+
+// set up map, view and base layer
+// set up the map
+var myMap;
+myMap = new L.Map('mapid', {
+	center: {lat: 0, lng: 8.8460}, 
+	zoom: 2,
+	layers: baseLayer
+});
+
 
 // add layer providers to map 
 
+// create layer group to add datasets too
+var datasets = L.layerGroup();
+
 // add GeoJSON dataset to map with jQuery ajax call
+
+var geoJsonFeature = {
+    "type": "Feature",
+    "properties": {
+        "name": "Leibniz-Zentrum für Marine Tropenökologie",
+        "type": "Research Institute",
+        "popupContent": "This is where the ZMT is located!"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [8.846278, 53.108312]
+    }
+};
--- static/tests/tests.js
+++ static/tests/tests.js
@@ -1,26 +1,34 @@
-QUnit.test("Test that the map's center is in the correct place?", function( assert ) {
+QUnit.test("map's center is in the correct place?", function( assert ) {
 	assert.equal(myMap.getCenter().toString(), "LatLng(0, 8.846)");
 });
 
-QUnit.test("Test that the map's zoom correct?", function( assert ) {
+QUnit.test("map's zoom is correct?", function( assert ) {
 	assert.equal(myMap.getZoom(), 2);
 });
 
-QUnit.test("Test base layer (osm) URL", function( assert ) {
+console.log(baseLayer());
+
+QUnit.test("check baseLayer layerGroup", function( assert ) {
+	assert.equal(baseLayer().getLayers().length, 1);
+});
+
+QUnit.test("base layer (osm) URL is correct", function( assert ) {
 	assert.equal(osm._url, "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png");
 });
 
-QUnit.test("Test base layer (osm) attribution?", function( assert ){
+QUnit.test("base layer (osm) attribution is correct", function( assert ){
 	assert.equal(osm.options.attribution, 
 		'&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>');
 });
 
-QUnit.test("Test base layer (osm) minZoom?", function( assert ){
+QUnit.test("base layer (osm) minZoom is correct", function( assert ){
 	assert.equal(osm.options.minZoom, 0);
 });
 
-QUnit.test("Test base layer (osm) maxZoom?", function( assert ){
+QUnit.test("base layer (osm) maxZoom is correct", function( assert ){
 	assert.equal(osm.options.maxZoom, 19);
 });
 
+QUnit.test("add dummy GeoJSON to map", function( assert ){
 
+});
