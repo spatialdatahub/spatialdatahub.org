56928b0c5381f21ef0b1ee45054b0dbbd8034419
Patrick Curry <patrick.curry@leibniz-zmt.de>
2016-11-04 12:01:27 +0100

    removing django's default admin stuff

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url, include
-from django.contrib import admin
+#from django.contrib import admin
 
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+#    url(r'^admin/', admin.site.urls),
 
     url(r'^',
         include('datasets.urls',
