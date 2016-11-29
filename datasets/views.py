7657ec4bf5a8fcd7545b7be54450f50468f54b0a
Patrick Curry <patrick.curry@leibniz-zmt.de>
2016-11-29 09:41:38 +0100

    removing encryption stuff

--- datasets/views.py
+++ datasets/views.py
@@ -5,8 +5,8 @@ from datasets.forms import DatasetForm
 
 from accounts.models import Account
 
-import os
-from cryptography.fernet import Fernet
+#import os
+#from cryptography.fernet import Fernet
 
 
 """
@@ -27,24 +27,7 @@ def new_dataset(request, account_slug=None):
     if request.method == "POST":
         form = DatasetForm(request.POST)
         if form.is_valid():
-            dataset = form.save(commit=False)
-            dataset.account = account
-
-            if dataset.dataset_user and dataset.dataset_password:
-                # get key (I am only using one key for both password and username)
-                cryptokey = os.environ["CRYPTOKEY"].encode("UTF-8")
-                cryptokey_fernet = Fernet(cryptokey)
-
-                # password
-                password_bytes = (dataset.dataset_password).encode("UTF-8")
-                password_encrypted = cryptokey_fernet.encrypt(password_bytes)
-                dataset.dataset_password = password_encrypted.decode("UTF-8")
-
-                # username
-                user_bytes = (dataset.dataset_user).encode("UTF-8")
-                user_encrypted = cryptokey_fernet.encrypt(user_bytes)
-                dataset.dataset_user = user_encrypted.decode("UTF-8")
-            dataset.save()
+            form.save()
             return redirect("accounts:account_detail",
                 account_slug=account.account_slug)
     else:
@@ -62,66 +45,10 @@ def dataset_update(request, account_slug=None, dataset_slug=None, pk=None):
     dataset = get_object_or_404(Dataset, dataset_slug=dataset_slug, pk=pk)
     if request.method == "POST":
         form = DatasetForm(request.POST, instance=dataset)
-        print(dataset.dataset_password)
         if form.is_valid():
             updated_dataset = form.save(commit=False)
             updated_dataset.account = account
-
-            #if dataset password and dataset user are not none go to next step
-                # if updated dataset password and updated dataset user are not
-                # none then go to next step
-                    # encrypt and save dataset user and password
-                # else updated dataset password and user equal old dataset
-                # password and usr
-            # else don't worry about it.
-            print(dataset.dataset_user + dataset.dataset_password)
-            if dataset.dataset_user != '' or None and dataset.dataset_password != '' or None:
-                print('dataset user and dataset password')
-                if updated_dataset.dataset_user and updated_dataset.dataset_password:
-                    print('updated dataset user and password entered')
-#                    cryptokey = os.environ["CRYPTOKEY"].encode("UTF-8")
-#                    cryptokey_fernet = Fernet(cryptokey)
-
-#                    password_bytes = (updated_dataset.dataset_password).encode("UTF-8")
-#                    password_encrypted = cryptokey_fernet.encrypt(password_bytes)
-#                    updated_dataset.dataset_password = password_encrypted.decode("UTF-8")
-
-#                    user_bytes = (updated_dataset.dataset_user).encode("UTF-8")
-#                    user_encrypted = cryptokey_fernet.encrypt(user_bytes)
-#                    updated_dataset.dataset_user = user_encrypted.decode("UTF-8")
-
-                elif updated_dataset.dataset_user == "" and updated_dataset.dataset_password == "":
-                    print('updated dataset user and password are not entered')
-                    updated_dataset.dataset_password = dataset.dataset_password
-                    updated_dataset.dataset_user = dataset.dataset_user
-#                    print(updated_dataset.dataset_password)
-            else:
-                updated_dataset.dataset_password = dataset.dataset_password
-                updated_dataset.dataset_user = dataset.dataset_user
-
             updated_dataset.save()
-
-#            if updated_dataset.dataset_user and updated_dataset.dataset_password:
-#                cryptokey = os.environ["CRYPTOKEY"].encode("UTF-8")
-#                cryptokey_fernet = Fernet(cryptokey)
-
-                # these won't work because the old password is encrypted, and
-                # doesn't show up in the form (because it's a secret password
-                # field)
-#                if updated_dataset.dataset_password != dataset.dataset_password:
-#                    password_bytes = (updated_dataset.dataset_password).encode("UTF-8")
-#                    password_encrypted = cryptokey_fernet.encrypt(password_bytes)
-#                    updated_dataset.dataset_password = password_encrypted.decode("UTF-8")
-
-                # these won't work because the old password is encrypted, and
-                # doesn't show up in the form (because it's a secret password
-                # field)
-#                if updated_datset.dataset_user != dataset.dataset_user:
-#                    user_bytes = (updated_dataset.dataset_user).encode("UTF-8")
-#                    user_encrypted = cryptokey_fernet.encrypt(user_bytes)
-#                    updated_dataset.dataset_user = user_encrypted.decode("UTF-8")
-
-#            updated_dataset.save()
             return redirect("datasets:dataset_detail",
                 account_slug=account.account_slug,
                 dataset_slug=dataset.dataset_slug,
--- main/views.py
+++ main/views.py
@@ -6,8 +6,8 @@ from datasets.models import Dataset
 from accounts.models import Account
 
 import requests
-import os
-from cryptography.fernet import Fernet
+#import os
+#from cryptography.fernet import Fernet
 
 
 
@@ -20,35 +20,9 @@ def load_dataset(request, pk):
     Also there needs to be a better way to hide the cryptokey
     """
     dataset = Dataset.objects.get(pk=pk)
-    if dataset.dataset_user and dataset.dataset_password:
-        # get cryptokey
-        cryptokey = os.environ["CRYPTOKEY"].encode("UTF-8")
-        cryptokey_fernet = Fernet(cryptokey)
-
-        # password
-        # turn convert password from string to bytes
-        # decrypt the dataset_password with the key
-        password_bytes = (dataset.dataset_password).encode("UTF-8")
-        password_decrypted_bytes = cryptokey_fernet.decrypt(password_bytes)
-        password_decrypted_string = password_decrypted_bytes.decode("UTF-8")
-
-        # user 
-        # turn convert user from string to bytes
-        # decrypt the dataset_user with the key
-        user_bytes = (dataset.dataset_user).encode("UTF-8")
-        user_decrypted_bytes = cryptokey_fernet.decrypt(user_bytes)
-        user_decrypted_string = user_decrypted_bytes.decode("UTF-8")
-
-        # finally make the request with the authentication password and
-        # username
-        r = requests.get(dataset.url, auth = (
-        user_decrypted_string, password_decrypted_string)).content
-        data = r
-    else:
-        r = requests.get(dataset.url).content
-        data = r
+    r = requests.get(dataset.url).content
+    data = r
     return HttpResponse(data)
-
 
 
 
