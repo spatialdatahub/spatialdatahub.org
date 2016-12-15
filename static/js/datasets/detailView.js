0942477aa718269dbbdb21b5f34812c52ab86d33
Patrick Curry <patrick.curry@leibniz-zmt.de>
2016-12-15 17:39:54 +0100

    adding filter selector function thing

--- static/js/datasets/detailView.js
+++ static/js/datasets/detailView.js
@@ -1,36 +1,44 @@
 // This is the javascript file for the map detail view
-// It is proving difficult to test because of mocking a dataset
 
-
-
-// when the dom is ready do all this stuff -- maybe...
-// this should be put into a function.
-
+// Define my variables
   let value = document.getElementById("mapid").getAttribute("value"),
-  ext = document.getElementById("mapid").getAttribute("ext");
+      ext = document.getElementById("mapid").getAttribute("ext"),
+      dsUrl = `/load_dataset/${value}`,
+      dataset,
+      featureCount = 0,
+      featureCountElement = document.getElementById('feature_count'),
+      featureSelector = document.getElementById('feature_selector');
 
-  var dataset;
-
+// define my popups function
   function onReadyPopups() {
     dataset.eachLayer( (layer) => {
+      featureCount++;
       let popupContent = []; 
       for (let key in layer.feature.properties) {
         popupContent.push(
           `<b>${key}</b>: ${layer.feature.properties[key]}`
         );
+        // add elements to property filter selector
+        featureSelector.options[featureSelector.options.length] = new Option(key);
+        // this doesn't work because it adds all elements for every single layer
+        // I only want it to run through a single layer and add it
       }
+
       if (layer.feature.geometry.type === "Point") {
         popupContent.push(`<b>Latitude:</b> ${layer.feature.geometry.coordinates[1]}`);
         popupContent.push(`<b>Longitude:</b> ${layer.feature.geometry.coordinates[0]}`);
       }
       layer.bindPopup(popupContent.join("<br/>"));
     });
+
     let bounds = dataset.getBounds();
     myMap.fitBounds(bounds);
+    // count features and add them to 'feature count html element'
+    featureCountElement.innerHTML = featureCountElement.innerHTML + ` ${featureCount}`;
   }
 
-// is there any reason to use this instead of an if else chain?
-  let dsUrl = `/load_dataset/${value}`
+// I might be able to add the .on and .addTo parts of this extensions to a function
+let typeSwitcher = () => {
   switch (ext) {
     case "kml":
       console.log('kml')
@@ -51,5 +59,11 @@
       .addTo(myMap);
       break;
   }
+};
 
+typeSwitcher();
+
+// It would be nice to get all the different features into a 'select' element or something,
+// and then use that to create a filter function
+// maybe it would be easiest to start with latitude and longitude
 
