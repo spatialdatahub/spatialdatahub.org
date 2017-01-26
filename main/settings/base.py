98388f6393285050339058dc91ff2e6f8975cca6
Patrick Curry <patrick.curry@leibniz-zmt.de>
2017-01-26 10:01:58 +0100

    trying to set up the travis and settings files to deal with travis and postgres

--- main/settings/base.py
+++ main/settings/base.py
@@ -85,16 +85,31 @@ WSGI_APPLICATION = "main.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "webgis_db",
-        "USER": "webgis_user",
-        "PASSWORD": "webgis_password",
-        "HOST": "localhost",
-        "PORT": "5432",
+################################################
+# set this thing up for travis and for gitlab-ci
+################################################
+if 'TRAVIS' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE':   'django.db.backends.postgresql_psycopg2',
+            'NAME':     'travisci',
+            'USER':     'postgres',
+            'PASSWORD': '',
+            'HOST':     'localhost',
+            'PORT':     '',
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql_psycopg2",
+            "NAME": "travis_ci_test",
+            "USER": "postgres",
+            "PASSWORD": "",
+            "HOST": "localhost",
+            "PORT": "5432",
+        }
+    }
 
 
 # Password validation
