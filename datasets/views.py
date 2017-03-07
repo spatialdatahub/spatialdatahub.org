75cb68b5339febf300f2e76bdedc8a83f7e11027
patcurry <currypat1985@gmail.com>
2017-03-07 11:54:37 +0100

    added user.id check and login_required to remove_dataset and updatei_dataset and update_dataset_auth views

--- datasets/views.py
+++ datasets/views.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.decorators import login_required
-from django.http import Http404
 from django.shortcuts import render, redirect, get_object_or_404
 
 from datasets.models import Dataset
@@ -39,60 +38,70 @@ def new_dataset(request, account_slug):
                   {"form": form,
                    "account": account})
 
+@login_required
 def dataset_update(request, account_slug=None, dataset_slug=None, pk=None):
     account = get_object_or_404(Account, account_slug=account_slug)
     dataset = get_object_or_404(Dataset, dataset_slug=dataset_slug, pk=pk)
-
-    if request.method == "POST":
-        form = DatasetUpdateForm(request.POST, instance=dataset)
-        if form.is_valid():
-            updated_dataset = form.save(commit=False)
-            updated_dataset.account = account
-            updated_dataset.save(update_fields=["title", "author", "url",
-                  "public_access", "description", "dataset_slug"])
-
-        return redirect("datasets:dataset_detail",
-                        account_slug=account.account_slug,
-                        dataset_slug=dataset.dataset_slug,
-                        pk=dataset.pk)
+    if request.user.id != account.user.id:
+        return redirect("access_denied")
     else:
-        form = DatasetUpdateForm(instance=dataset)
-    template_name = "datasets/dataset_update.html"
-    return render(request, template_name,
-                  {"form": form,
-                   "account": account,
-                   "dataset": dataset})
+        if request.method == "POST":
+            form = DatasetUpdateForm(request.POST, instance=dataset)
+            if form.is_valid():
+                updated_dataset = form.save(commit=False)
+                updated_dataset.account = account
+                updated_dataset.save(update_fields=["title", "author", "url",
+                      "public_access", "description", "dataset_slug"])
 
+            return redirect("datasets:dataset_detail",
+                            account_slug=account.account_slug,
+                            dataset_slug=dataset.dataset_slug,
+                            pk=dataset.pk)
+        else:
+            form = DatasetUpdateForm(instance=dataset)
+        template_name = "datasets/dataset_update.html"
+        return render(request, template_name,
+                      {"form": form,
+                       "account": account,
+                       "dataset": dataset})
+
+@login_required
 def dataset_update_auth(request, account_slug=None, dataset_slug=None, pk=None):
     account = get_object_or_404(Account, account_slug=account_slug)
     dataset = get_object_or_404(Dataset, dataset_slug=dataset_slug, pk=pk)
-
-    if request.method == "POST":
-        form = DatasetUpdateAuthForm(request.POST, instance=dataset)
-        if form.is_valid():
-            updated_dataset = form.save(commit=False)
-            updated_dataset.account = account
-            updated_dataset.save()
-
-        return redirect("datasets:dataset_detail",
-                        account_slug=account.account_slug,
-                        dataset_slug=dataset.dataset_slug,
-                        pk=dataset.pk)
+    if request.user.id != account.user.id:
+        return redirect("access_denied")
     else:
-        form = DatasetUpdateAuthForm(instance=dataset)
-    template_name = "datasets/dataset_update_auth.html"
-    return render(request, template_name,
-                  {"form": form,
-                   "account": account,
-                   "dataset": dataset})
+        if request.method == "POST":
+            form = DatasetUpdateAuthForm(request.POST, instance=dataset)
+            if form.is_valid():
+                updated_dataset = form.save(commit=False)
+                updated_dataset.account = account
+                updated_dataset.save()
 
+            return redirect("datasets:dataset_detail",
+                            account_slug=account.account_slug,
+                            dataset_slug=dataset.dataset_slug,
+                            pk=dataset.pk)
+        else:
+            form = DatasetUpdateAuthForm(instance=dataset)
+        template_name = "datasets/dataset_update_auth.html"
+        return render(request, template_name,
+                      {"form": form,
+                       "account": account,
+                       "dataset": dataset})
+
+@login_required
 def dataset_remove(request, account_slug=None, dataset_slug=None, pk=None):
     account = get_object_or_404(Account, account_slug=account_slug)
     dataset = get_object_or_404(Dataset, dataset_slug=dataset_slug, pk=pk)
     context = {"account": account, "dataset": dataset}
     template_name = "datasets/dataset_remove.html"
-    if request.method == "POST":
-        dataset.delete()
-        return redirect("accounts:account_detail",
-                        account_slug=account.account_slug)
-    return render(request, template_name, context)
+    if request.user.id != account.user.id:
+        return redirect("access_denied")
+    else:
+        if request.method == "POST":
+            dataset.delete()
+            return redirect("accounts:account_detail",
+                            account_slug=account.account_slug)
+        return render(request, template_name, context)
