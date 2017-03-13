d6ebe05c6127426dc8d1665bfa87230aac6cbd2a
patcurry <currypat1985@gmail.com>
2017-03-13 15:24:13 +0100

    the turf stuff isn't really working just yet

--- static/js/datasets/detailView.js
+++ static/js/datasets/detailView.js
@@ -53,8 +53,12 @@ L.control.homebutton({position: 'topleft'}).addTo(myMap)
 L.control.togglescrollbutton = (options) => new L.Control.ToggleScrollButton(options)
 L.control.togglescrollbutton({position: 'topleft'}).addTo(myMap)
 
+// ////////////////////////////////////////////////////////////////////////////
+/*
+// PAGE SPECIFIC FUNCTIONS
+*/
+// ////////////////////////////////////////////////////////////////////////////
 
-// Page Specific JS
 const dataset = []
 const pk = document.getElementById('dataset_pk').getAttribute('value')
 const ext = document.getElementById('dataset_ext').getAttribute('value')
@@ -96,21 +100,135 @@ const filteredLayer = L.geoJSON(null, {
 // could I just pass the dataset and object in here, instead
 // of in the index.js page?
 // or could i just put this on the index page and do it there?
+
 const fc = obj => {
   featureCountElement.innerHTML = Object.keys(obj._layers).length
 }
 
-getDataset(url, ext, myMap, dataset, pk, filteredLayer, fc)
 
 
+// Select correct function with the extension, then get the data
+// with a promise. Then add the data to the modified layer as geoJson,
+// then add the modified layer to the map and get its bounds
+// then call any other functions that should be run when the page is loaded
 
-/* instead of making a monster function with a bunch of arguements, do it sequentially */
-// 1) check the ext and select the omnivore function
-selectOmnivore(ext)
+extSelect(ext, url)
+  .then(response => {
+    dataset.push(response.toGeoJSON()) // add data to empty dataset array
+    filteredLayer.addData(response.toGeoJSON()) // add data to filter layer
+    myMap.addLayer(filteredLayer)
+      .fitBounds(filteredLayer.getBounds()) // add filter layer to map
+    fc(filteredLayer) // get the number of features and add it to the feature count div
+  }, error => console.log(error))
 
-// 2) use omnivore to get data
 
-// 3) then do stuff with the data
+// Time to add turf
+// first get points for polygon
+const submitValuesButton = document.getElementById('submit_values_button')
+
+const minLatInput = document.getElementById('min_lat_input')
+const maxLatInput = document.getElementById('max_lat_input')
+const minLngInput = document.getElementById('min_lng_input')
+const maxLngInput = document.getElementById('max_lng_input')
+
+// add event listener to submit button
+submitValuesButton.addEventListener('click', () => {
+
+  // get values for (square) polygon
+  minLat = minLatInput.value
+  maxLat = maxLatInput.value
+  minLng = minLngInput.value
+  maxLng = maxLngInput.value
+
+  // make default values for inputs
+
+  if (minLng === '') {
+    minLng = -180
+  } else if (minLng <= -180) {
+    minLng = -180
+  } else if (minLng >= 180) {
+    minLng = 180
+  }
+ 
+  if (maxLng === '') {
+    maxLng = 180
+  } else if (maxLng <= -180) {
+    maxLng = -180
+  } else if (maxLng >= 180) {
+    maxLng = 180
+  }
+
+  if (minLat === '') {
+    minLat = -90
+  } else if (minLat <= -90) {
+    minLat = -90
+  } else if (minLat >= 90) {
+    minLat = 90
+  }
+
+  if (maxLat === '') {
+    maxLat = 90
+  } else if (maxLat <= -90) {
+    maxLat = -90
+  } else if (maxLat >= 90) {
+    maxLat = 90
+  }
+
+
+  // make polygon and add it to a feature collection
+  // - values for lng are not working. try something else
+
+  const poly = turf.featureCollection([
+    turf.polygon([[
+      [minLng, maxLat], // same as end
+      [maxLng, maxLat],
+      [maxLng, minLat],
+      [minLng, minLat],
+      [minLng, maxLat] // same as beginning
+    ]])
+  ])
+
+/*
+var features = [
+  turf.point([minLng, maxLat]),
+  turf.point([maxLng, maxLat]),
+  turf.point([maxLng, minLat]),
+  turf.point([minLng, minLat])
+]
+
+var poly = turf.featureCollection(features)
+*/
+
+console.log(poly)
+
+/*
+  // make polygon with 
+  const poly = turf.polygon([[
+      [-50, 50], // same as end
+      [50, 50],
+      [50, -50],
+      [-50, -50],
+      [-50, 50] // same as beginning
+    ]])
+*/
+  
+
+  L.geoJSON(poly).addTo(myMap)
+
+  // clear filteredLayer
+  filteredLayer.clearLayers()
+
+  // get data within polygon
+  const ds = dataset[0]
+  const withinData = turf.within(ds, poly)
+
+  console.log(withinData)
+
+  // add data to filtered Layer
+  filteredLayer.addData(withinData)
+})
+
+
 
 
 /*
