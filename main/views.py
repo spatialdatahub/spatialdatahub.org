32da756f25e3a9c98e954a54bfcebb66ddd41771
patcurry <currypat1985@gmail.com>
2017-03-15 11:26:27 +0100

    took the 'if dataset.dataset_password' out of main/views.py'

--- main/views.py
+++ main/views.py
@@ -21,36 +21,32 @@ def load_dataset(request, pk):
     """
     # bring in the dataset by pk
     dataset = Dataset.objects.get(pk=pk)
-    if dataset.dataset_password:
-        # the basedir/filepath stuff can probably be done better
-        # set base dir
-        base_dir = os.path.dirname(
-            os.path.dirname(os.path.abspath(__file__)))
-        # get key from file
-        f = base_dir + "/temp_password.txt"
-        g = open(f)
-        key = g.read().encode("utf-8")
-        g.close()
-        cipher_end = Fernet(key)
+    # the basedir/filepath stuff can probably be done better
+    # set base dir
+    base_dir = os.path.dirname(
+        os.path.dirname(os.path.abspath(__file__)))
+    # get key from file
+    f = base_dir + "/temp_password.txt"
+    g = open(f)
+    key = g.read().encode("utf-8")
+    g.close()
+    cipher_end = Fernet(key)
 
-        # this would probably go better with a little loop or something
-        bytes_user = dataset.dataset_user.encode("utf-8")
-        bytes_password = dataset.dataset_password.encode("utf-8")
-        decrypted_user = cipher_end.decrypt(bytes_user).decode("utf-8")
-        decrypted_password = cipher_end.decrypt(bytes_password).decode("utf-8")
+    # this would probably go better with a little loop or something
+    bytes_user = dataset.dataset_user.encode("utf-8")
+    bytes_password = dataset.dataset_password.encode("utf-8")
+    decrypted_user = cipher_end.decrypt(bytes_user).decode("utf-8")
+    decrypted_password = cipher_end.decrypt(bytes_password).decode("utf-8")
 
-        # use owncloud stuff, if the owncloud thing is true
-        if dataset.owncloud:
-            # owncloud, instead of requests.
-            oc = owncloud.Client(dataset.owncloud_instance)
-            oc.login(decrypted_user, decrypted_password)
-            data = oc.get_file_contents(dataset.owncloud_path)
-
-        else:
-            data = requests.get(dataset.url,
-                             auth=(decrypted_user, decrypted_password)).content
+    # use owncloud stuff, if the owncloud thing is true
+    if dataset.owncloud:
+        # owncloud, instead of requests.
+        oc = owncloud.Client(dataset.owncloud_instance)
+        oc.login(decrypted_user, decrypted_password)
+        data = oc.get_file_contents(dataset.owncloud_path)
     else:
-        data = requests.get(dataset.url).content
+        data = requests.get(dataset.url,
+                            auth=(decrypted_user, decrypted_password)).content
 
     return HttpResponse(data)
 
