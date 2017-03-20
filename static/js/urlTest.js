705bff9d85384962e998c1cad51c61efaf1c1c9f
patcurry <currypat1985@gmail.com>
2017-03-19 17:52:16 -0700

    slow progress

---
+++ static/js/urlTest.js
@@ -0,0 +1,80 @@
+// Start with a bunch of stuff from other libraries, then add code from my own libraries
+const osm = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: `&copy; <a href="http://www.openstreetmap.org/copyright">
+  OpenStreetMap</a>`,
+  minZoom: 2,
+  maxZoom: 19
+})
+
+const stamenToner = L.tileLayer(`http://stamen-tiles-{s}.a.ssl.fastly.net/
+toner/{z}/{x}/{y}.{ext}`, {
+  attribution: `Map tiles by <a href="http://stamen.com">Stamen Design</a>,
+  <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>
+  &mdash; Map data &copy;
+  <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>`,
+  subdomains: 'abcd',
+  minZoom: 2,
+  maxZoom: 19,
+  ext: 'png'
+})
+
+const esriWorldImagery = L.tileLayer(`http://server.arcgisonline.com/ArcGIS/
+rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}`, {
+  attribution: `Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA,
+  USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP,
+  and the GIS User Community`
+})
+
+const myMap = L.map('mapid', {
+  center: {lat: 0, lng: 8.8460},
+  zoom: 2,
+  layers: osm,
+  scrollWheelZoom: false
+})
+
+const baseLayers = {
+  'Open Street Maps': osm,
+  'Black and White': stamenToner,
+  'ESRI World Map': esriWorldImagery
+}
+
+const baseLayerControl = L.control.layers(baseLayers)
+baseLayerControl.addTo(myMap)
+
+// watermark leaflet control
+L.control.watermark = (options) => new L.Control.Watermark(options)
+L.control.watermark({position: 'bottomleft'}).addTo(myMap)
+
+// home button leaflet control
+L.control.homebutton = (options) => new L.Control.HomeButton(options)
+L.control.homebutton({position: 'topleft'}).addTo(myMap)
+
+// toggle scroll button leaflet control
+L.control.togglescrollbutton = (options) => new L.Control.ToggleScrollButton(options)
+L.control.togglescrollbutton({position: 'topleft'}).addTo(myMap)
+
+// ////////////////////////////////////////////////////////////////////////////
+/*
+// PAGE SPECIFIC FUNCTIONS
+*/
+// ////////////////////////////////////////////////////////////////////////////
+
+// there must be a better way to do this... but for now it works
+// it should be turned into a function and used here and in the portalView
+
+
+
+const urlInput = document.getElementById('url_test')
+
+urlInput.addEventListener('input', () => console.log(this))
+
+// url.endsWith('json') ? ext = 'geojson' : console.log('url')
+
+/*
+extSelect(ext, url)
+  .then(response => {
+    filteredLayer.addData(response.toGeoJSON()) // add data to filter layer
+    myMap.addLayer(filteredLayer)
+      .fitBounds(filteredLayer.getBounds()) // add filter layer to map
+  }, error => console.log(error))
+*/
