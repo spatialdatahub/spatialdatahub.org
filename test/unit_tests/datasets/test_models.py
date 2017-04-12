e72559ad9f1f80e065fadce52285d3d6ae36958a
patcurry <currypat1985@gmail.com>
2017-04-12 14:55:10 +0200

    separating keywords tests from dataset models tests

--- test/unit_tests/datasets/test_models.py
+++ test/unit_tests/datasets/test_models.py
@@ -4,7 +4,6 @@ from django.db.utils import IntegrityError
 from django.test import TestCase
 
 from datasets.models import Dataset
-from datasets.models import Keyword
 from accounts.models import Account
 
 from cryptography.fernet import Fernet
@@ -170,72 +169,4 @@ class DatasetModelTests(TestCase):
         decrypted_user = cipher_end.decrypt(bytes_user).decode("utf-8")
         self.assertEqual(decrypted_user, "zmtdummy")
 
-# This won't come until after I fix my tests. There aren't any account tests
-
-class KeywordModelTests(TestCase):
-    def setUp(self):
-
-        self.u1 = User.objects.create_user(
-            username="test_user", password="test_password")
-
-        self.a1 = self.u1.account
-        self.a1.affiliation = "Zentrum für Marine Tropenökologie"
-        self.a1.save()
-
-        self.ds1 = Dataset.objects.create(
-            account=self.a1,
-            author="Google",
-            title="Google GeoJSON Example",
-            description="Polygons spelling 'GOOGLE' over Australia",
-            url="https://storage.googleapis.com/maps-devrel/google.json",
-            public_access=True)
-
-        self.ds2 = Dataset.objects.create(
-           account=self.a1,
-           author="zmtdummy",
-           title="Password Protected Dataset",
-           description="Just a page that requires login and password info",
-           url="https://bitbucket.org/zmtdummy/geojsondata/raw/" +
-               "ad675d6fd6e2256b365e79e785603c2ab454006b/" +
-               "password_protected_dataset.json",
-           dataset_user="zmtdummy",
-           dataset_password="zmtBremen1991",
-           public_access=False)
-
-    def test_that_keyword_can_be_created(self):
-        kw1 = Keyword.objects.create(keyword="biology")
-        self.assertEqual(kw1, Keyword.objects.get(keyword="biology"))
-
-    def test_that_keyword_can_be_associated_with_dataset(self):
-        kw1 = Keyword.objects.create(keyword="biology")
-        kw1.datasets.add(self.ds1)
-        kw1.save()
-
-        self.assertEqual(kw1.datasets.all().first(), self.ds1)
-
-    def test_that_keyword_can_be_associated_with_multiple_datasets(self):
-        kw1 = Keyword.objects.create(keyword="biology") 
-        kw1.datasets.add(self.ds1, self.ds2)
-        kw1.save()
-
-        self.assertEqual(len(kw1.datasets.all()), 2)
-
-    def test_that_dataset_can_be_associated_with_multiple_keywords(self):
-        kw1 = Keyword.objects.create(keyword="biology")
-        kw1.datasets.add(self.ds1)
-        kw1.save()
-        kw2 = Keyword.objects.create(keyword="chemistry")
-        kw2.datasets.add(self.ds1)
-        kw2.save()
-
-        self.assertEqual(len(self.ds1.keyword_set.all()), 2)
-
-    def test_keywords_must_be_unique(self):
-        with self.assertRaises(IntegrityError):
-            kw1 = Keyword.objects.create(keyword="biology")
-            kw1.datasets.add(self.ds1)
-            kw1.save()
-            kw2 = Keyword.objects.create(keyword="biology")
-            kw2.datasets.add(self.ds1)
-            kw2.save()
 
