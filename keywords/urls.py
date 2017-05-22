b179148ae17f3868c163670292321f1a2426adb1
patcurry <currypat1985@gmail.com>
2017-05-22 16:00:03 +0200

    removed "new_keyword" view/template/url from keywords, they can only be made from datasets now

--- keywords/urls.py
+++ keywords/urls.py
@@ -9,10 +9,6 @@ urlpatterns = [
         views.keyword_list,
         name="keyword_list"),
 
-    url(r'^new_keyword/$',
-        views.new_keyword,
-        name="new_keyword"),
-
     url(r'^(?P<keyword_slug>[-\w]*)/$',
         views.keyword_datasets,
         name="keyword_datasets"),
--- keywords/views.py
+++ keywords/views.py
@@ -19,48 +19,16 @@ def keyword_list(request):
         keyword_list = Keyword.objects.all().order_by("keyword")
 
     total_keywords = len(keyword_list)
-#    total_datasets = 0
-
-# this is incorrect math, it will double count datasets that are associated
-# with more than one keyword. Having this count is less than important
-#    for kw in keyword_list:
-#        total_datasets += len(kw.datasets.all())
 
     template_name = "keywords/keyword_list.html"
     return render(request, template_name,
                   {"keyword_list": keyword_list,
                    "total_keywords": total_keywords})
-#                  "total_keywords": total_keywords,
-#                  "total_datasets": total_datasets})
-
 
 def keyword_datasets(request, keyword_slug=None):
-    """
-    This checks if there is a GET request with a 'q' key, and then, if there
-    is, it returns the list of q items as a list. Once there is the list,
-    it can go filter the datasets by keyword primary key. I don't know if this
-    would work with fields other than the primary key field. If there is not a
-    request.GET item, it just returns all the datasets.
-    """
     keyword = get_object_or_404(Keyword, keyword_slug=keyword_slug)
     dataset_list = keyword.datasets.all()
     template_name = "keywords/keyword_datasets.html"
     return render(request, template_name,
                   context={"keyword": keyword, "dataset_list": dataset_list})
-
-
-def new_keyword(request):
-    """
-    Gotta figure out a good way to make new keywords and associate them with
-    the right datasets.
-    """
-    if request.method == "POST":
-        form = KeywordCreateForm(request.POST)
-        if form.is_valid():
-            form.save()
-    form = KeywordCreateForm()
-    template_name = "keywords/new_keyword.html"
-    return render(request, template_name, {"form": form})
-
-
 
