a95d24151dae7994a35ab877d1343a1c9fda51d5
patcurry <currypat1985@gmail.com>
2017-05-22 12:28:11 +0200

    taking care of username case sensitivity with customusercreation form, I cannot figure out how to do it without, that will have to work for now

--- core/forms.py
+++ core/forms.py
@@ -1,9 +1,18 @@
 from django.contrib.auth.forms import UserCreationForm
-from core.models import CustomUser
+from django.contrib.auth import get_user_model
 
+
+User = get_user_model()
 
 class CustomUserCreationForm(UserCreationForm):
 
     class Meta(UserCreationForm.Meta):
-        model = CustomUser
+        model = User
         fields = UserCreationForm.Meta.fields + ('email',)
+
+    def clean(self):
+        cleaned_data = super(CustomUserCreationForm, self).clean()
+        username = cleaned_data.get('username')
+        if username and User.objects.filter(username__iexact=username).exists():
+            self.add_error('username', 'A user with that username already exists.')
+        return cleaned_data
--- core/models.py
+++ core/models.py
@@ -3,9 +3,12 @@ from django.db import models
 
 
 class CustomUserManager(UserManager):
+    """Something is missing, this isn't catching the problem"""
+
     def get_by_natural_key(self, username):
-        case_insensitive_username = '{}__iexact'.format(self.model.USERNAME_FIELD)
-        return self.get(**{case_insensitive_username: username})
+        #case_insensitive_username = '{}__iexact'.format(self.model.USERNAME_FIELD)
+        #return self.get(**{case_insensitive_username: username})
+        return self.get(username__iexact = username)
 
 
 class CustomUser(AbstractUser):
