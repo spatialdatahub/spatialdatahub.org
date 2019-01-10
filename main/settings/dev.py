314624e87c6e8d533c6cb6ff3030de79473b2480
patcurry <currypat1985@gmail.com>
2019-01-10 16:21:05 +0100

    got this working on the local computer with postgres, now have to try and get it to work with travis

--- main/settings/dev.py
+++ main/settings/dev.py
@@ -12,9 +12,22 @@ DEBUG = True
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+"""
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'spatialdatahub_db',
+        'USER': 'spatialdatahub_user',
+        'PASSWORD': 'spatialdatahub_password',
+        'HOST': '',
+        'PORT': '5432'
+        }
+    }
