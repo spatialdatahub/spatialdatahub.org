2d1f4a9349cc8efb81ea7625e56be4823a49d380
patcurry <currypat1985@gmail.com>
2019-01-10 16:34:25 +0100

    going to try this out

--- main/settings/dev.py
+++ main/settings/dev.py
@@ -12,6 +12,8 @@ DEBUG = True
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+# It would be nice to use sqlite3 but it's not doing nice things with python
+# and django right now
 """
 DATABASES = {
     'default': {
@@ -21,13 +23,25 @@ DATABASES = {
 }
 """
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'spatialdatahub_db',
-        'USER': 'spatialdatahub_user',
-        'PASSWORD': 'spatialdatahub_password',
-        'HOST': '',
-        'PORT': '5432'
+if 'TRAVIS' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE':   'django.db.backends.postgresql_psycopg2',
+            'NAME':     'travisci',
+            'USER':     'postgres',
+            'PASSWORD': '',
+            'HOST':     'localhost',
+            'PORT':     '',
         }
     }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'spatialdatahub_db',
+            'USER': 'spatialdatahub_user',
+            'PASSWORD': 'spatialdatahub_password',
+            'HOST': '',
+            'PORT': '5432'
+            }
+        }
