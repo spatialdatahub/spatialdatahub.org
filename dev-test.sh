4e5bfbd02571fa47d5e2a9ba71c7e116bac12312
Pat <16329318+patcurry@users.noreply.github.com>
2019-02-12 15:16:28 +0100

    Dev (#14)
    * nothing in Dockerfile, but it is there to be populated
    
    * added ENV to .gitignore list
    
    * i have a development start script now
    
    * set up a quick development test shell script
    
    * added development test shell script and put it into the travis file
    
    * added python 3.5 and 3.7 to versions, how do i make sure that it does not try to deploy from master branch on forked version?
    
    * trying to get python 3.7 to run with travis
    
    * trying to figure out how to use zmt email stuff
    
    * there is an untracked file called "other-secrets.json"
    
    * Double quotes were being retained in environmental variables from json, it was a jq (command line tool) issue
    
    * Dev (#13)
    
    * yeah
    
    * had to put some more variables into dev-test.sh (#2)
    
    * had to put some more variables into dev-test.sh
    
    * cleaning up dev-start.sh and dev-test.sh
    
    * I realized that .travis was using dev-test.sh, but a necessary config file was not under version control, i fixed this by modifying the .travis.yml
    
    *  fixing .travis.yml environment issues
    
    * hit and miss
    
    * got the .travis.yml working with the correct env variables

---
+++ dev-test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+python3 -m venv ENV
+source ENV/bin/activate
+
+# set secret environmental variables
+export SECRET_KEY=`jq '.SECRET_KEY' dev-secrets.json`
+export CRYPTO_KEY=`jq '.CRYPTO_KEY' dev-secrets.json`
+
+export EMAIL_HOST=`jq -r '.EMAIL_HOST' other-secrets.json`
+export EMAIL_PORT=`jq -r '.EMAIL_PORT' other-secrets.json`
+export EMAIL_HOST_USER=`jq -r '.EMAIL_HOST_USER' other-secrets.json`
+export EMAIL_HOST_PASSWORD=`jq -r '.EMAIL_HOST_PASSWORD' other-secrets.json`
+export DEFAULT_FROM_EMAIL=`jq -r '.DEFAULT_FROM_EMAIL' other-secrets.json`
+
+pip install --upgrade pip
+pip install -r requirements.txt
+
+#python manage.py migrate
+python manage.py test --settings=main.settings.dev -v2
